8059b602503e4d8823273f6abfe8d50273277ae2
Kid Kid <kidkid.gum@gmail.com>
2022-12-13 00:02:29 -0600

    improved list api call.

--- DemoApp/list/api/MealListRequestBuilder.swift
+++ DemoApp/list/api/MealListRequestBuilder.swift
@@ -5,15 +5,25 @@
 //  Created by Kid Kid on 12/7/22.
 //
 
-struct MealListRequestBuilder { }
+struct MealListRequestBuilder {
+    let category: String
+}
 
 extension MealListRequestBuilder: DecodableRequestBuilding {
     var path: String {
-        return "filter.php?c=Dessert"
+        return "filter.php"
     }
     
     var apiVersion: String {
         return "json/v1/1/"
+    }
+    
+    var parameters: [String : AnyObject]? {
+        return ["c": category] as? [String: AnyObject]
+    }
+    
+    var encoding: RequestEncoding {
+        return .url
     }
     
     var method: RequestMethod {
--- DemoApp/list/api/MealListService.swift
+++ DemoApp/list/api/MealListService.swift
@@ -11,16 +11,18 @@ protocol MealListServing {
 
 struct MealListService {
     let apiManager: APIManaging
+    let category: String
     
-    init(apiManager: APIManaging) {
+    init(apiManager: APIManaging, category: String) {
         self.apiManager = apiManager
+        self.category = category
     }
 }
 
 extension MealListService: MealListServing {
     func loadMeals() async throws -> [Meal] {
         try await withCheckedThrowingContinuation { continuation in
-            let req = MealListRequestBuilder()
+            let req = MealListRequestBuilder(category: category)
             let res = MealListResponseParser(continuation: continuation)
             apiManager.perform(of: MealsResponse.self, requestBuilder: req, responseParser: res)
         }
